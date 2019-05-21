aba4fceeb03a345c58092d87caaf2a61d4b32cd3
Piiman <palomar.miguel2d@gmail.com>
2019-05-21 02:54:42 -0500

    añado puto maiz
    ewyhgsrtyuhsrth

---
+++ CSR/Assets/Scripts/Sjugador.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sjugador : MonoBehaviour
+{
+
+    public int puntos;
+	public Rigidbody2D rbd;
+	static float maxSteel = 13f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntos = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((rbd.velocity.magnitude) <= maxSteel)
+        {
+            if (rbd.velocity.y < 0)
+            {
+                rbd.AddForce(new Vector2(0,-10));
+            }
+            if (rbd.velocity.y < 0)
+            {
+                rbd.AddForce(new Vector2(0, 10));
+            }
+        }
+            if (Input.GetKey("space")){
+        	rbd.AddForce(new Vector2(0,75));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("moneda"))
+        {
+            other.gameObject.SetActive(false);
+            puntos++;
+        }
+    }
+}
---
+++ CSR/Assets/Scripts/Smaizito.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smaizito : MonoBehaviour
+{
+
+    public Rigidbody2D rbd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbd.velocity = new Vector2(-5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
