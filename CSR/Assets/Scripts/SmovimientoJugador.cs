2fb920d0d99d7032a74b5d89cd0692cd937b5312
Piiman <palomar.miguel2d@gmail.com>
2019-05-21 01:15:50 -0500

    arreglo problema con el movimiento de personaje

--- CSR/Assets/Scripts/SmovimientoJugador.cs
+++ CSR/Assets/Scripts/SmovimientoJugador.cs
@@ -18,10 +18,20 @@ public class SmovimientoJugador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("space")){
-        	if((rbd.velocity.magnitude) <= maxSteel){
-        		rbd.AddForce(new Vector2(0,75));
-        	}
+
+        if ((rbd.velocity.magnitude) <= maxSteel)
+        {
+            if (rbd.velocity.y < 0)
+            {
+                rbd.AddForce(new Vector2(0,-10));
+            }
+            if (rbd.velocity.y < 0)
+            {
+                rbd.AddForce(new Vector2(0, 10));
+            }
+        }
+            if (Input.GetKey("space")){
+        	rbd.AddForce(new Vector2(0,75));
         }
     }
 }
