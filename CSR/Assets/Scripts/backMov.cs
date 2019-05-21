97e4e3132dd6cf85cff1e87b88e6b7b41577795e
NMayhem <psnisawii@gmail.com>
2019-05-21 01:44:24 -0500

    Añadido background movil

---
+++ CSR/Assets/Scripts/backMov.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backMov : MonoBehaviour
+{
+
+    Material mat;
+    Vector2 offset;
+
+    public int velX, velY;
+
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(velX, velY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
