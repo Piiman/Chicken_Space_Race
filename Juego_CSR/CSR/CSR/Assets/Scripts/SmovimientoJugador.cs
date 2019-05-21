2c248b48261667d1898a129bf5b9a839db8e9f95
Yisus <victorf9523@gmail.com>
2019-05-21 00:15:17 -0500

    ya quedo

---
+++ Juego_CSR/CSR/CSR/Assets/Scripts/SmovimientoJugador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmovimientoJugador : MonoBehaviour
+{
+
+
+	public Rigidbody2D rbd;
+	static float maxSteel = 13f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("space")){
+        	if((rbd.velocity.magnitude) <= maxSteel){
+        		rbd.AddForce(new Vector2(0,75));
+        	}
+        }
+    }
+}
